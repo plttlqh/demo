fd873ce2e17a0309729f4f829a7aefc31cd700ff
hale <quangha@smartdev.vn>
2016-05-26 15:06:36 +0700

    comparing

---
+++ src/main/java/com/example/testing/comparing/Duck.java
@@ -0,0 +1,28 @@
+package com.example.testing.comparing;
+
+public class Duck implements Comparable {
+    String name;
+    int weight;
+
+    public Duck(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        Duck otherDuck = (Duck) o;
+        if(this.weight < otherDuck.weight){
+            return -1;
+        } else if(this.weight == otherDuck.weight){
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return name +" weight=" + weight;
+    }
+}
